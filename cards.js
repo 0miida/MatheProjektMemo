b0312f85514e68542a01a0bd5e14211eeacc614b
Steinhartermann <steinhartermann@gmail.com>
2025-06-17 10:49:41 +0200

    Create cards.js

---
+++ cards.js
@@ -0,0 +1,13 @@
+// cards.js
+export const cards = [
+  { id: 1, name: "🍎" },
+  { id: 2, name: "🍌" },
+  { id: 3, name: "🍇" },
+  { id: 4, name: "🍉" },
+  { id: 5, name: "🍓" },
+  { id: 6, name: "🍒" },
+  { id: 7, name: "🥝" },
+  { id: 8, name: "🍍" },
+  { id: 9, name: "🥭" },
+  { id: 10, name: "🍑" }
+];
